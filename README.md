bda0470bea3483c1b2d9cf7714c77c46fdd5ece2
q-kazukiniimura <kazukiniimura@quantexa.com>
2024-04-04 14:21:53 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# jp-address-parser
-It is a Japanese address parser by regex
+# Japanese Address Parser
+This Python script is designed to parse Japanese address strings provided in a CSV file (`data/full_address.csv`). The parsed addresses are then exported to another CSV file (`data/parsed_address.csv`). The script consists of several functions to handle the import, parsing, and export processes.
+
+## Usage:
+1. Ensure that the `data/full_address.csv` file containing the address data is present.
+2. Install requrired libraries below:
+``` shell:
+pip install normalize_japanese_addresses
+```
+3. Execute the script below:
+```shell:
+python jpAddressParser.py
+```
+
+## Requirements:
+* Python 3.x
+* [normalize_japanese_addresses](https://pypi.org/project/normalize-japanese-addresses/)
+* CSV file (`full_address.csv`) containing Japanese address data in the specified format.
---
+++ jpAddressParser.py
@@ -0,0 +1,86 @@
+import csv
+import re
+
+from normalize_japanese_addresses import normalize
+
+
+def import_csv(file_path: str) -> list:
+    """ Read a CSV file from the path and return a list of dictionaries """
+    data_list = []
+    with open(file_path, 'r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+                data_list.append({'full_address': row[0], 'work': row[0]})
+    return data_list
+
+def export_csv(data: list, file_path: str):
+    try:
+        header = [*data[0].keys()]
+        with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def extract_country(addr: dict) -> dict:
+    if '日本' in addr['work']:
+        addr['work'] = addr['work'].replace('日本', '')
+        addr['country'] = '日本'
+    else:
+        addr['country'] = None
+    return addr
+
+def extract_postalcode(addr: dict) -> dict:
+    postal_code_pattern = r'(〒?\d{3}-\d{4}|〒?\d{7})'
+
+    match = re.search(postal_code_pattern, addr.get('work', ''))
+    if match:
+        postal_code = match.group()
+        addr['work'] = re.sub(postal_code_pattern, '', addr['work'])
+        addr['PostalCode'] = postal_code
+    else:
+        addr['PostalCode'] = None
+
+    return addr
+
+def parse_address(addr: dict) -> dict:
+    parsed = normalize(addr['work'])
+    addr['prefecture'] = parsed['pref']
+
+    county = None
+    ward = None
+    city = parsed['city']
+
+    if '郡' in city:
+        country = city.split('郡')[0] + '郡'
+        city = city.split('郡')[-1]
+    elif parsed['pref'] != '東京都' and '区' in city:
+        ward = city.split('区')[0] + '区'
+        city = city.split('区')[-1]
+
+    addr.update({'county': county, 'city': city, 'ward': ward})
+
+    addr['neighborhood'] = parsed['town']
+    addr['banch'] = parsed['addr']
+    addr['go'] = parsed['addr']
+    addr['buildingName'] = None
+    addr['roomNumber'] = None
+    addr['floorNumber'] = None
+    del addr['work']
+
+    return addr
+
+
+if __name__ == "__main__":
+    # CSV data import
+    rec = import_csv("data/full_address.csv")
+    # Extract country
+    rec = [extract_country(r) for r in rec]
+    # Extract postalcode
+    rec = [extract_postalcode(r) for r in rec]
+    # parse address
+    rec = [parse_address(r) for r in rec]
+    # Export CSV
+    export_csv(rec, "data/parsed_address.csv")
+
